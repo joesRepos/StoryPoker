adffd6562fa082b610c8daad065c070c73ae0ebc
joesRepos <joemitchell1717@gmail.com>
2025-11-28 16:34:11 +0000

    Login page structure.

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -4,7 +4,23 @@ import React, {useEffect, useState} from 'react';
 export default function LoginPage() {
     const navigate = useNavigate();
 
+    function CreateVote() {
+        console.log(document.getElementById("title").value);
+    }
+
+    function EnterVote() {
+        console.log("Entering vote: " + document.getElementById("title").value);
+    }
     return <div className="login-page">
-        <h1>Login Page</h1>
+        <h2>Enter your name</h2>
+        <input type="text" id = "name" placeholder="Name" required/>
+
+        <h2>Vote Title</h2>
+        <input type="text" id = "title" placeholder="Name" required/>
+        <p></p>
+        <button type="button" id="button" onClick={CreateVote}>New Vote</button>
+        <button type="button" id="button" onClick={EnterVote}>Enter Vote</button>
+
+
     </div>; 
 }
