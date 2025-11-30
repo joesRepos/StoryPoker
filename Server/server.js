3d5b6651eda6488cac2f6bea839eab121d078e60
joesRepos <joemitchell1717@gmail.com>
2025-11-30 11:40:05 +0000

    API calls to sevrer to find the vote id with the name of the room and vice versa.

--- Server/server.js
+++ Server/server.js
@@ -15,6 +15,40 @@ app.post("/api/create-new-vote", async (req, res) => {
         console.log("Error creating new vote: " + error);
         res.json("INVALID");
     }
-    
 });
+
+app.post("/api/get-vote-id", async (req, res) => {
+
+    try {
+        const voteName = req.body.data;
+        for (let i = 0; i < votes.length; i++) {
+            if (voteName === votes[i]) {
+                console.log("Vote ID found.")
+                res.json(i);
+            }
+        }
+        console.log("No vote found.");
+        res.json("Address Unknown.");
+    } catch (error) {
+        console.log("Error finding vote id: " + error);
+        res.json("INVALID");
+    }
+});
+
+app.post("/api/get-vote-name", async (req, res) => {
+
+    try {
+        const voteID = req.body.data;
+        if (voteID < votes.length) {
+            console.log("Vote Name found.")
+            res.json(votes[voteID]);
+        }
+        console.log("No vote found.");
+        res.json("Address Unknown.");
+    } catch (error) {
+        console.log("Error finding vote name: " + error);
+        res.json("INVALID");
+    }
+});
+
 app.listen(5000,() => {console.log("Server started on port 5000")});
