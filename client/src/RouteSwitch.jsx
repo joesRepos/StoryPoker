1b86e33b6649b310b9b10a1b0bf0d1bb11080739
joesRepos <joemitchell1717@gmail.com>
2025-12-04 21:07:24 +0000

    Adding the Vote Page.

--- client/src/RouteSwitch.jsx
+++ client/src/RouteSwitch.jsx
@@ -1,12 +1,14 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 import LoginPage from './pages/LoginPage';
+import LoginPage from './pages/VotePage';
 
 const RouteSwitch = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<LoginPage/>}/>
+                <Route path="/vote-page" element={<LoginPage/>}/>
             </Routes>
         </BrowserRouter>
     )
---
+++ client/src/pages/VotePage.jsx
@@ -0,0 +1,8 @@
+import { useNavigate } from "react-router-dom";
+import React, {useEffect, useState} from 'react';
+
+export default function LoginPage() {
+    const navigate = useNavigate();
+
+    return <div className="vote-page">Vote Page</div>;
+}
